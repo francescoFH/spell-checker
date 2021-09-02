13276d1991ccee241efa1e68e5018d7c02afb79e
francescoFH <francescofacca@gmail.com>
2021-09-02 12:22:07 +0100

    Added failing test to check that a correct word is returned as it is

--- spec/spell_spec.rb
+++ spec/spell_spec.rb
@@ -2,4 +2,10 @@ require "spell"
 
 describe Spell do
 
+  subject(:spell) { described_class.new }
+
+  it "returns correctly spelled word as it is" do
+    expect(spell.check("Hello")).to eq "Hello"
+  end
+
 end
