f0a1db88162811dbd7b8cb02883d268e78630df1
francescoFH <francescofacca@gmail.com>
2021-09-06 16:38:33 +0100

    Added failing test to check that words of a sentence included in word_bank array are returned as they are

--- spec/spell_spec.rb
+++ spec/spell_spec.rb
@@ -4,16 +4,24 @@ describe Spell do
 
   subject(:spell) { described_class.new }
 
-  it "returns correctly spelled word as it is" do
-    expect(spell.check("Hello")).to eq "Hello"
+  context "correctly spelled word(s)" do
+    it "returns correctly spelled word as it is" do
+      expect(spell.check("Hello")).to eq "Hello"
+    end
+
+    it "returns words of a sentence as they are if they match entries in the word_bank" do
+      expect(spell.check("Hello word")).to eq "Hello word"
+    end
+  end
+
+  context "incorrectly spelled word(s)" do
+    it "returns a word surrounded by ~ if it doesn't match a word in the word_bank array" do
+      expect(spell.check('a')).to eq '~a~'
+    end
   end
 
   it "contains word bank when initialised" do
     expect(spell.word_bank).to be_a Array
-  end
-
-  it "return a word surrounded by ~ if it doesn't match a word in the word_bank array" do
-    expect(spell.check('a')).to eq '~a~'
   end
 
 end
