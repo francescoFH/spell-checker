2fe7b8ee8ac92ea53776b819f95955de18fd175b
francescoFH <francescofacca@gmail.com>
2021-09-06 23:58:10 +0100

    Added feature on check method to map each word of a sentence to check if words are included in word_bank

--- lib/spell.rb
+++ lib/spell.rb
@@ -2,15 +2,25 @@ class Spell
   attr_reader :word_bank
 
   def initialize
-    @word_bank = ["Hello"]
+    @word_bank = ["Hello", "word"]
   end
 
   def check(string)
-    if word_bank.include?(string)
-      return string
-    else
-      return "~#{string}~"
-    end
+
+    words = string.split(" ")
+
+    new_words = words.map { |word|
+
+      if word_bank.include?(word)
+        word = word
+      else
+        word = "~#{word}~"
+      end
+
+    }
+
+    return new_words.join(" ")
+
   end
 
 end
